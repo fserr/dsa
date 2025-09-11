a8aae813858bebca7d2ac825367a45f8412e8f77
Federico Serra <fserra8@gmail.com>
2025-09-11 09:54:27 +0200

    +circular queue

--- cqueue.py
+++ cqueue.py
@@ -1,55 +1,50 @@
-''' 
-
-Standard queue:
-    > popping an item means reducing the size of the queue
+'''
+Circular queue:
+    > last items point to the first => size isn't reduced by popping elems
 
 Complexity (array impl):
-    > Queue & deque --> O(1)
-
+    >
 '''
 
-class Queue():
+class CQueue():
     
     def __init__(self, size):
         self.size = size
         self.queue = [None] * size
         self.head = self.tail = -1
-    
+
     def enqueue(self, item):
-        if (self.tail == self.size - 1):
-            print("The queue is full.")
+        if ((self.tail + 1) % self.size == self.head):
+            print("The cqueue is full.")
         elif (self.head == -1):
             self.head = 0
             self.tail = 0
             self.queue[self.tail] = item
         else:
-            self.tail += 1
+            self.tail = (self.tail + 1) % self.size
             self.queue[self.tail] = item
 
     def dequeue(self):
         if (self.head == -1):
             print("The queue is empty.")
-        elif (self.tail == self.head):
-            tmp = self.queue[self.head]
+        elif (self.head == self.tail):
             self.head = -1
             self.tail = -1
-            return tmp
         else:
-            tmp = self.queue[self.head]
-            self.head += 1
+            tmp = self.head
+            self.head = (self.head + 1) % self.size
             return tmp
 
     def print_queue(self):
         if (self.head == -1):
-            print("The queue is empty")
-        else:
+            print("The queue is empty.")
+        elif (self.tail >= self.head):
             for i in range(self.head, self.tail + 1):
                 print(self.queue[i], end=" ")
             print()
-
-    def print_current_size(self):
-        if (self.head == -1):
-            print("The queue is empty.")
         else:
-            print(f"Queue current size: {self.size - self.head}")
-
+            for i in range(self.head, self.size):
+                print(self.queue[i], end=" ")
+            for i in range (0, self.tail + 1):
+                print(self.queue[i], end=" ")
+            print()
--- main.py
+++ main.py
@@ -1,22 +1,46 @@
 from stack import *
 from queue import *
+from cqueue import *
 
 def main():
+    # CQUEUE
+    myqueue = CQueue(3)
+    myqueue.enqueue(1)
+    myqueue.enqueue(2)
+    myqueue.enqueue(3)
+    myqueue.enqueue(4)
+
+    myqueue.print_queue()
+
+    myqueue.dequeue()
+    myqueue.dequeue()
+    myqueue.enqueue(4)
+    myqueue.enqueue(5)
+    myqueue.enqueue(6)
+
+    myqueue.print_queue()
+
+
     # --- QUEUE ---
-    my_queue = Queue(3)
-    
-    my_queue.enqueue(1)
-    my_queue.enqueue(2)
-    my_queue.enqueue(3)
-    my_queue.enqueue(4)
+   # my_queue = Queue(3)
+   # 
+   # my_queue.enqueue(1)
+   # my_queue.enqueue(2)
+   # my_queue.enqueue(3)
+   # my_queue.enqueue(4)
+   # my_queue.print_queue()
+   # my_queue.print_current_size()
+   # 
+   # my_queue.dequeue()
+   # my_queue.dequeue()
 
-    my_queue.print_queue()
+   # my_queue.print_queue()   
+   # my_queue.print_current_size()
 
-    my_queue.dequeue()
-    my_queue.dequeue()
+   # my_queue.enqueue(1)
+   # my_queue.enqueue(1)
 
-    my_queue.print_queue()
-    
+   # my_queue.print_current_size()
     # --- STACK ---
     # my_stack = stack_create()
     # stack_push(my_stack, 1)
